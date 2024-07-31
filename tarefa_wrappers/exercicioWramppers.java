5021898ee51631d9a26c43922a4c242df3ca4c96
ueliton silva <ueliton45@hotmail.com>
2024-07-31 10:38:17 -0300

    exercicio-de-casting

---
+++ tarefa_wrappers/exercicioWramppers.java
@@ -0,0 +1,14 @@
+package tarefa_wrappers;
+
+public class exercicioWramppers {
+    public static void main(String args[] ) {
+
+        int numPrimitivo = 256;
+
+        System.out.println("Valor primitivo: " + numPrimitivo);
+
+        Integer wrapperInt = numPrimitivo;
+
+        System.out.println("Valor Wrapper : " + wrapperInt);
+    }
+}
